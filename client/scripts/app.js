8bb63edea9491b2ad6f12e40b12cb5c0f5abed20
meiflin123 <meiflin123@gmail.com>
2018-06-25 17:34:14 -0700

    complete skikel

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -6,6 +6,7 @@ var Movie = Backbone.Model.extend({
 
   toggleLike: function() {
     // your code here
+    this.set('like', false);
   }
 
 });
@@ -15,13 +16,15 @@ var Movies = Backbone.Collection.extend({
   model: Movie,
 
   initialize: function() {
-    // your code here
+    this.on('change:like', this.sort, this);
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
     // your code here
+    this.comparator = field;
+    this.sort();
   }
 
 });
@@ -59,6 +62,10 @@ var MovieView = Backbone.View.extend({
 
   initialize: function() {
     // your code here
+    debugger;
+    this.model.on('change', function() {
+      this.render();
+    }, this);
   },
 
   events: {
@@ -66,7 +73,7 @@ var MovieView = Backbone.View.extend({
   },
 
   handleClick: function() {
-    // your code here
+    this.model.toggleLike();
   },
 
   render: function() {
@@ -79,7 +86,7 @@ var MovieView = Backbone.View.extend({
 var MoviesView = Backbone.View.extend({
 
   initialize: function() {
-    // your code here
+    this.collection.on('sort', this.render, this);
   },
 
   render: function() {
